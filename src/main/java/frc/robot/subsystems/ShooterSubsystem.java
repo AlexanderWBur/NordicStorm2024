d0583ac993d902439c46ad529943dfc673948f36
AlexanderWBur <abur@stpeterschools.org>
2024-02-10 10:31:13 -0600

    Add barrel property

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -13,15 +13,15 @@ import frc.robot.RobotContainer;
 
 public class ShooterSubsystem extends SubsystemBase {
 
-private CANSparkMax bottomWheel = new CANSparkMax(Constants.lowerFlywheelID, MotorType.kBrushless);
-private SparkPIDController bottomWheelPID = bottomWheel.getPIDController();
-private RelativeEncoder bottomWheelEncoder = bottomWheel.getEncoder();
+    private CANSparkMax bottomWheel = new CANSparkMax(Constants.lowerFlywheelID, MotorType.kBrushless);
+    private SparkPIDController bottomWheelPID = bottomWheel.getPIDController();
+    private RelativeEncoder bottomWheelEncoder = bottomWheel.getEncoder();
 
-private CANSparkMax topWheel = new CANSparkMax(Constants.upperFlywheelID, MotorType.kBrushless);
-private SparkPIDController topWheelPID = topWheel.getPIDController();
-private RelativeEncoder topWheelEncoder = topWheel.getEncoder();
+    private CANSparkMax topWheel = new CANSparkMax(Constants.upperFlywheelID, MotorType.kBrushless);
+    private SparkPIDController topWheelPID = topWheel.getPIDController();
+    private RelativeEncoder topWheelEncoder = topWheel.getEncoder();
 
-    public ShooterSubsystem(){
+    public ShooterSubsystem() {
         configureFlywheelMotor(topWheel);
         configureFlywheelMotor(bottomWheel);
 
@@ -34,15 +34,15 @@ private RelativeEncoder topWheelEncoder = topWheel.getEncoder();
     }
 
     @Override
-    public void periodic(){
+    public void periodic() {
         updateShooter(); // top then bottom
-        setFlywheelsRaw(.25* RobotContainer.leftJoystick.getY(),-.4 *RobotContainer.leftJoystick.getY());
+        setFlywheelsRaw(.25 * RobotContainer.leftJoystick.getY(), -.4 * RobotContainer.leftJoystick.getY());
     }
 
     private double topCurrentRPM;
     private double bottomCurrentRPM;
 
-    public void setFlywheelsRaw(double top, double bottom){
+    public void setFlywheelsRaw(double top, double bottom) {
         topWheelPID.setReference(top, CANSparkMax.ControlType.kDutyCycle);
         bottomWheelPID.setReference(bottom, CANSparkMax.ControlType.kDutyCycle);
 
@@ -52,29 +52,24 @@ private RelativeEncoder topWheelEncoder = topWheel.getEncoder();
         topCurrentRPM = topWheelEncoder.getVelocity();
         bottomCurrentRPM = bottomWheelEncoder.getVelocity();
 
-        SmartDashboard.putNumber("Top wheel velocity",topCurrentRPM);
+        SmartDashboard.putNumber("Top wheel velocity", topCurrentRPM);
         SmartDashboard.putNumber("Bottom wheel velocity", bottomCurrentRPM);
-
 
     }
 
     private double topTargetRPM;
     private double bottomTargetRPM;
 
-    public void updateFlywheels(double topRPM, double bottomRPM){
-        if(topRPM < 0 || bottomRPM < 0) {
+    public void updateFlywheels(double topRPM, double bottomRPM) {
+        if (topRPM < 0 || bottomRPM < 0) {
             System.out.println("Error tried to set shooter to negative RPM");
         }
 
-    topTargetRPM = topRPM;
-       bottomTargetRPM = bottomRPM;
-
-    
-
+        topTargetRPM = topRPM;
+        bottomTargetRPM = bottomRPM;
 
         topWheelPID.setReference(topRPM, CANSparkMax.ControlType.kVelocity);
         bottomWheelPID.setReference(bottomRPM, CANSparkMax.ControlType.kVelocity);
 
     }
 }
-
